4edc69dae20927241eb89d5cdd778e8cadee0b24
Squares <26125768+RectangularObject@users.noreply.github.com>
2024-04-15 06:10:46 -0400

    reconfigure UI

--- src/modules/UI.lua
+++ src/modules/UI.lua
@@ -9,7 +9,7 @@ local UI = {
 }
 
 function UI:Load()
-	local mainWindow = LinoriaLib:CreateWindow({Title = "Universal Hitbox Expander", TabPadding = 6, MenuFadeTime = 0, Size = UDim2.fromOffset(550, 620)})
+	local mainWindow = LinoriaLib:CreateWindow({ Title = "Universal Hitbox Expander", TabPadding = 6, MenuFadeTime = 0, Size = UDim2.fromOffset(550, 620) })
 	local mainTab = mainWindow:AddTab("Main")
 
 	local hitboxGroup = mainTab:AddLeftGroupbox("Hitbox Expander")
@@ -17,13 +17,13 @@ function UI:Load()
 	hitboxGroup:AddToggle("collisionsToggle", { Text = "Collisions" })
 	hitboxGroup:AddSlider("hitboxSize", { Text = "Size", Min = 2, Max = 100, Default = 5, Rounding = 0 })
 	hitboxGroup:AddSlider("hitboxTransparency", { Text = "Transparency", Min = 0, Max = 1, Default = 0.5, Rounding = 2 })
-	hitboxGroup:AddInput("customPart", { Text = "Custom Part Name", Default = "HeadHB" })
+	hitboxGroup:AddInput("customPartName", { Text = "Custom Part Name", Default = "HeadHB" })
 	hitboxGroup:AddDropdown("hitboxPartList", {
 		Text = "Body Parts",
 		AllowNull = true,
 		Multi = true,
-		Values = { "Custom Part", "Head", "HumanoidRootPart", "Torso", "Left Arm", "Right Arm", "Left Leg", "Right Leg" },
-		Default = "HumanoidRootPart",
+		Values = { "Custom Part", "Head", "PrimaryPart", "Torso", "Left Arm", "Right Arm", "Left Leg", "Right Leg" },
+		Default = "PrimaryPart",
 	})
 
 	local espGroup = mainTab:AddLeftGroupbox("ESP")
@@ -45,19 +45,17 @@ function UI:Load()
 	miscGroup:AddButton("Unload", LinoriaLib.Unload)
 
 	local ignoresGroup = mainTab:AddRightGroupbox("Ignores")
-	ignoresGroup:AddToggle("ignoreSitting", { Text = "Ignore Sitting Players" })
+	ignoresGroup:AddToggle("ignoreTeammates", { Text = "Ignore Teammates" })
 	ignoresGroup:AddToggle("ignoreFF", { Text = "Ignore Forcefielded Players" })
+	ignoresGroup:AddToggle("ignoreSitting", { Text = "Ignore Sitting Players" })
 	ignoresGroup:AddToggle("ignoreSelectedPlayers", { Text = "Ignore Selected Players" })
 	ignoresGroup:AddDropdown("ignorePlayerList", { Text = "Players", SpecialType = "Player", Multi = true })
-	ignoresGroup:AddToggle("ignoreOwnTeam", { Text = "Ignore Own Team" })
 	ignoresGroup:AddToggle("ignoreSelectedTeams", { Text = "Ignore Selected Teams" })
 	ignoresGroup:AddDropdown("ignoreTeamList", { Text = "Teams", SpecialType = "Team", Multi = true })
 
 	LinoriaLib.ToggleKeybind = UI.Options.menuKeybind
 	SaveManager:BuildConfigSection(mainTab)
-
-	LinoriaLib:Notify("hai :3", nil, 8549385246)
-	LinoriaLib:Notify(`Press {LinoriaLib.ToggleKeybind.Value} to open the menu`, nil, 8549385246)
+	SaveManager:LoadAutoloadConfig()
 
 	return self
 end
