c2ec721424ab2788069235d94d7bb354e26e05f0
Squares <26125768+RectangularObject@users.noreply.github.com>
2024-04-15 06:43:26 -0400

    fix init and add onchanged connections

--- src/init.lua
+++ src/init.lua
@@ -5,19 +5,32 @@ local HitboxHandler = require("./modules/HitboxHandler.lua")
 local UI = require("./modules/UI.lua")
 local VisualHandler = require("./modules/VisualHandler.lua")
 local gameoverride = require("./modules/Overrides.lua")
-local lib = UI.Library
 
 UI:Load()
 EntityHandler:Load()
 VisualHandler:Load()
 HitboxHandler:Load()
 
-lib:Notify("hai :3", nil, 8549385246)
-lib:Notify(`Press {lib.ToggleKeybind.Value} to open the menu`, nil, 8549385246)
-if gameoverride then lib:Notify("This game has custom support!", nil, 8549385246) end
-
-lib:OnUnload(function()
-	VisualHandler:Unload()
+UI.Library:OnUnload(function()
 	HitboxHandler:Unload()
+	VisualHandler:Unload()
 	EntityHandler:Unload()
 end)
+
+UI.Toggles.hitboxToggle:OnChanged(HitboxHandler.updateHitbox)
+UI.Toggles.collisionsToggle:OnChanged(HitboxHandler.updateHitbox)
+UI.Options.hitboxSize:OnChanged(HitboxHandler.updateHitbox)
+UI.Options.hitboxTransparency:OnChanged(HitboxHandler.updateHitbox)
+UI.Options.customPartName:OnChanged(HitboxHandler.updateHitbox)
+UI.Options.hitboxPartList:OnChanged(HitboxHandler.updateHitbox)
+UI.Toggles.ignoreTeammates:OnChanged(HitboxHandler.updateHitbox)
+UI.Toggles.ignoreFF:OnChanged(HitboxHandler.updateHitbox)
+UI.Toggles.ignoreSitting:OnChanged(HitboxHandler.updateHitbox)
+UI.Toggles.ignoreSelectedPlayers:OnChanged(HitboxHandler.updateHitbox)
+UI.Options.ignorePlayerList:OnChanged(HitboxHandler.updateHitbox)
+UI.Toggles.ignoreSelectedTeams:OnChanged(HitboxHandler.updateHitbox)
+UI.Options.ignoreTeamList:OnChanged(HitboxHandler.updateHitbox)
+
+UI.Library:Notify("hai :3", nil, 8549385246)
+UI.Library:Notify(`Press {UI.Library.ToggleKeybind.Value} to open the menu`, nil, 8549385246)
+if gameoverride then UI.Library:Notify("This game has custom support!", nil, 8549385246) end
