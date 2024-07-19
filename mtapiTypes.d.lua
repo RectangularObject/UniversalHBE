0b3f4f6c71ff6008054b03378606aa3605b29aa5
Squares <26125768+RectangularObject@users.noreply.github.com>
2024-07-19 01:57:43 -0400

    update mtapi types

--- mtapiTypes.d.lua
+++ mtapiTypes.d.lua
@@ -1,16 +1,20 @@
 -- https://marketplace.visualstudio.com/items?itemName=JohnnyMorganz.luau-lsp
+declare class mtapiHook
+    function Remove(self): ()
+    function Modify(self, value: any): ()
+end
 declare class BasePart extends BasePart
-    function AddGetHook(self, property: string, value: any)
-    function AddSetHook(self, property: string, value: any)
-    function AddCallHook(self, functionName: string, callback: (...any) -> ...any)
+    function AddGetHook(self, property: string, value: any): mtapiHook
+    function AddSetHook(self, property: string, value: any): mtapiHook
+    function AddCallHook(self, functionName: string, callback: (...any) -> ...any): mtapiHook
 end
 declare class Decal extends Decal
-    function AddGetHook(self, property: string, value: any)
-    function AddSetHook(self, property: string, value: any)
-    function AddCallHook(self, functionName: string, callback: (...any) -> ...any)
+    function AddGetHook(self, property: string, value: any): mtapiHook
+    function AddSetHook(self, property: string, value: any): mtapiHook
+    function AddCallHook(self, functionName: string, callback: (...any) -> ...any): mtapiHook
 end
 declare game: typeof(game) & {
-    AddGlobalGetHook: (self: DataModel, property: string, value: any) -> (),
-    AddGlobalSetHook: (self: DataModel, property: string, value: any) -> (),
-    AddGlobalCallHook: (self: DataModel, functionName: string, callback: (...any) -> ...any) -> (),
+    AddGlobalGetHook: (self: DataModel, property: string, value: any) -> mtapiHook,
+    AddGlobalSetHook: (self: DataModel, property: string, value: any) -> mtapiHook,
+    AddGlobalCallHook: (self: DataModel, functionName: string, callback: (...any) -> ...any) -> mtapiHook,
 }
