1bd0d240421a1c026c77e689af6cb380ec33f0f4
Squares <26125768+RectangularObject@users.noreply.github.com>
2024-07-24 22:07:52 -0400

    create basic npc funcs

--- modules/EntityHandler.lua
+++ modules/EntityHandler.lua
@@ -1,11 +1,15 @@
+local Entity = require("./Classes/Entity.lua")
 local Event = require("./Classes/Event.lua")
 local Player = require("./Classes/Player.lua")
 local Players = cloneref(game:GetService("Players"))
 local entityHandler = {
 	PlayerAdded = Event.new(),
 	PlayerRemoving = Event.new(),
+	EntityAdded = Event.new(),
+	EntityRemoving = Event.new(),
 }
 local playerList = {}
+local entityList = {}
 local connections = {}
 
 local function PlayerAdded(plr)
@@ -16,6 +20,14 @@ local function PlayerRemoving(plr)
 	entityHandler.PlayerRemoving:Fire(playerList[plr])
 	playerList[plr] = nil
 end
+local function EntityAdded(ent)
+	entityList[ent] = Entity.new(ent)
+	entityHandler.EntityAdded:Fire(entityList[ent])
+end
+local function EntityRemoving(ent)
+	entityHandler.EntityAdded:Fire(entityList[ent])
+	entityList[ent] = nil
+end
 
 function entityHandler:Load()
 	for i, plr in ipairs(Players:GetPlayers()) do
@@ -25,7 +37,10 @@ function entityHandler:Load()
 	table.insert(connections, Players.PlayerAdded:Connect(PlayerAdded))
 	table.insert(connections, Players.PlayerRemoving:Connect(PlayerRemoving))
 end
+function entityHandler:addEntity(ent) EntityAdded(ent) end
+function entityHandler:removeEntity(ent) EntityRemoving(ent) end
 function entityHandler:GetPlayers() return playerList end
+function entityHandler:GetEntities() return entityList end
 function entityHandler:Unload()
 	for _, connection in connections do
 		connection:Disconnect()
